262f5f5be91fc4ee6d9343a558aaca73b4aafdce
Наталья Борисова <borisovan.work@gmail.com>
2021-01-02 01:23:17 +0400

    commit test for button sdd to basket

--- test_items.py
+++ test_items.py
@@ -1,1 +1,7 @@
 shop_link =  'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_should_see_add_to_basket(browser):
+    browser.implicitly_wait(5)
+    browser.get(shop_link)
+    count_button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
+    assert count_button > 0, 'add to basket button is not found'
